6d6cbef5a6fb39486755900fbcd08eb04dc600cb
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2020-04-06 00:26:25 +0200

    Added inCombat tracker - tutorial end

--- SRAO.lua
+++ SRAO.lua
@@ -2,10 +2,16 @@ SRAO = {}
 
 SRAO.name = "SRAO"
 
+isLoaded = false
+
 function Initialize()
   SRAO.inCombat = IsUnitInCombat("player")
+  SRAOIndicator:SetHidden(not SRAO.inCombat)
 
   EVENT_MANAGER:RegisterForEvent(SRAO.name, EVENT_PLAYER_COMBAT_STATE, OnPlayerCombatState)
+
+  SRAO.savedVariables = ZO_SavedVars:New("SRAO_SavedVariables", 1, nil, {})
+  RestorePosition()
 end
 
 function OnAddOnLoaded(event, addonName)
@@ -14,24 +20,37 @@ function OnAddOnLoaded(event, addonName)
   end
 
   Initialize()
+  isLoaded = true
 end
 
 function OnPlayerActivated(eventCode)
-  d("|ccc0000S|cff3300R|cff9933A|cff9966O|r addon loaded.")
+  if(isLoaded) then
+    d("|ccc0000S|cff3300R|cff9933A|cff9966O|r addon loaded.")
+  end
 end
 
 function OnPlayerCombatState(eventCode, inCombat)
-  if inCombat ~= SRAO.inCombat then
+  if inCombat ~= SRAO.inCombat or isLoaded then
     SRAO.inCombat = inCombat
 
-    if inCombat then
-      d("|ccc0000Entering combat.")
-    else
-      d("Exiting combat.")
-    end
-
+    SRAOIndicator:SetHidden(not inCombat)
   end
+end
+
+function OnIndicatorMoveStop()
+  SRAO.savedVariables.left = SRAOIndicator:GetLeft()
+  SRAO.savedVariables.top = SRAOIndicator:GetTop()
+end
+
+function RestorePosition()
+  local left = SRAO.savedVariables.left
+  local top = SRAO.savedVariables.top
+
+  SRAOIndicator:ClearAnchors()
+  SRAOIndicator:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
 end
 
 EVENT_MANAGER:RegisterForEvent(SRAO.name, EVENT_ADD_ON_LOADED, OnAddOnLoaded)
 EVENT_MANAGER:RegisterForEvent(SRAO.name, EVENT_PLAYER_ACTIVATED, OnPlayerActivated)
+
+--GetCurrencyAmount(CURT_EVENT_TICKETS, CURRENCY_LOCATION_ACCOUNT)
