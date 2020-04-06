b3307f9dc33e5536609e997a595c5caed6170464
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2020-04-07 00:22:55 +0200

    Warning now hides if any menu gets opened

--- EventTickets/EventTickets.lua
+++ EventTickets/EventTickets.lua
@@ -1,9 +1,11 @@
 -- Currency Tracking File
+local EventTickets = {}
 
 function InitializeCurrencyTracker()
 
   --Event ticket warning restore init check
-  --ShowEventTicketsWarning(GetCurrencyAmount(CURT_EVENT_TICKETS, CURRENCY_LOCATION_ACCOUNT) >= 10)
+  EventTickets.displayWarning = (GetCurrencyAmount(CURT_EVENT_TICKETS, CURRENCY_LOCATION_ACCOUNT) >= 10)
+  ShowEventTicketsWarning(EventTickets.displayWarning)
 
   --Event tickets warning restore
   local left = CurrencyTracker.savedVariables.EventTicketsWarningLeft
@@ -15,19 +17,31 @@ end
 
 function OnCurrencyUpdate(eventCode, currencyType, currencyLocation, newAmount, oldAmount, reason)
   if(currencyType == CURT_EVENT_TICKETS) then
-
     --Detect if event tickets quantity >= 10
-    ShowEventTicketsWarning(newAmount >= 10)
+    EventTickets.displayWarning = (newAmount >= 10)
+    ShowEventTicketsWarning(true)
   end
 end
 
 function ShowEventTicketsWarning(show)
-  GUI_EventTicketsWarning:SetHidden(not show)
+  GUI_EventTicketsWarning:SetHidden(not show or not EventTickets.displayWarning)
 end
 
 function OnIndicatorMoveStopEventTicketsWarning()
-  --CurrencyTracker.savedVariables.EventTicketsWarningLeft = GUI_EventTicketsWarning:GetLeft()
-  --CurrencyTracker.savedVariables.EventTicketsWarningTop = GUI_EventTicketsWarning:GetTop()
+  CurrencyTracker.savedVariables.EventTicketsWarningLeft = GUI_EventTicketsWarning:GetLeft()
+  CurrencyTracker.savedVariables.EventTicketsWarningTop = GUI_EventTicketsWarning:GetTop()
 end
 
+function layerPopped(eventCode, layerIndex, activeLayerIndex)
+  --Show Warning when going back to game
+  ShowEventTicketsWarning(activeLayerIndex == 2)
+end
+
+function layerPushed(eventCode, layerIndex, activeLayerIndex)
+  --Hide Warning when opening any menu
+  ShowEventTicketsWarning(activeLayerIndex == 2)
+end
+
+EVENT_MANAGER:RegisterForEvent(CurrencyTracker.name, EVENT_ACTION_LAYER_POPPED, layerPopped)
+EVENT_MANAGER:RegisterForEvent(CurrencyTracker.name, EVENT_ACTION_LAYER_PUSHED, layerPushed)
 EVENT_MANAGER:RegisterForEvent(CurrencyTracker.name, EVENT_CURRENCY_UPDATE, OnCurrencyUpdate)
