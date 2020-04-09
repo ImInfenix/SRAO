3c4cb81bf339e9013e1c92942c2374c83065726d
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2020-04-09 19:51:14 +0200

    Added AddonMenu, option to display addon loaded information

---
+++ Menu.lua
@@ -0,0 +1,40 @@
+--Addon Settings Menu File
+local LAM = LibAddonMenu2
+
+function InitializeLAM()
+	--LibAddonMenu-2.0 Setup
+	local saveData = CurrencyTracker.savedVariables
+	--Test if addon is launched for first time for each value picked by user
+	if saveData.displayAddonLoadedMessage == nil then saveData.displayAddonLoadedMessage = true end
+
+	local settingsPanel
+	local settingsPanelName = CurrencyTracker.name .. "SettingsPanel"
+	local settingsPanelData = {
+	  type = "panel",
+	  name = CurrencyTracker.name,
+	  author = CurrencyTracker.author,
+	  version = CurrencyTracker.version,
+	  website = CurrencyTracker.website
+	}
+	local settingsOptionsData = {
+		{
+			type = "header",
+			name = "general"
+		},
+		{
+			type = "checkbox",
+			name = "Enable start message",
+			getFunc = function() return saveData.displayAddonLoadedMessage end,
+			setFunc = function(value)
+						saveData.displayAddonLoadedMessage = value
+			end
+		},
+		{
+			type = "header",
+			name = "general mais moins quand meme"
+		}
+	}
+
+	settingsPanel = LAM:RegisterAddonPanel(settingsPanelName, settingsPanelData)
+	LAM:RegisterOptionControls(settingsPanelName, settingsOptionsData)
+end
