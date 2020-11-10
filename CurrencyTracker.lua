9445d08c9f2059e4ba8444436aeeb552b8ec7220
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2020-11-10 23:10:38 +0100

    Events are now handled through a more logic way

--- CurrencyTracker.lua
+++ CurrencyTracker.lua
@@ -7,37 +7,39 @@ CurrencyTracker.author = "|cff6600Infenix|r"
 CurrencyTracker.version = "1.0"
 CurrencyTracker.website = "https://github.com/ImInfenix/CurrencyTracker"
 
-local isLoaded = false
-
 function CurrencyTracker.Initialize()
   CurrencyTracker.savedVariables = ZO_SavedVars:NewAccountWide("CurrencyTracker_SavedVariables", 1, nil, {})
   CurrencyTracker.displayAddonLoadedMessage = CurrencyTracker.savedVariables.displayAddonLoadedMessage
 
+  --LibAddonMenu-2.0 Initializing
+  CurrencyTracker.InitializeLAM()
+
   --EventTickets Initializing
   CurrencyTracker.InitializeEventTickets()
 
-  --LibAddonMenu-2.0 Initializing
-  CurrencyTracker.InitializeLAM()
+  EVENT_MANAGER:RegisterForEvent(CurrencyTracker.name, EVENT_PLAYER_ACTIVATED, CurrencyTracker.OnPlayerActivated)
 end
 
 function CurrencyTracker.OnAddOnLoaded(eventCode, addonName)
-  if addonName ~= CurrencyTracker.name or isLoaded then
+  if(addonName ~= CurrencyTracker.name or isLoaded) then
     return
   end
-  
+
   --Unregister for load event
   EVENT_MANAGER:UnregisterForEvent(addonName, eventCode)
 
   CurrencyTracker.Initialize()
-  isLoaded = true
 end
 
 function CurrencyTracker.OnPlayerActivated(eventCode)
-  if(isLoaded and CurrencyTracker.displayAddonLoadedMessage) then
+
+    --Unregister for player activated event
+    EVENT_MANAGER:UnregisterForEvent(addonName, eventCode)
+
+  if(CurrencyTracker.displayAddonLoadedMessage) then
     d(CurrencyTracker.coloredName .. " addon loaded.")
     CurrencyTracker.displayAddonLoadedMessage = false
   end
 end
 
 EVENT_MANAGER:RegisterForEvent(CurrencyTracker.name, EVENT_ADD_ON_LOADED, CurrencyTracker.OnAddOnLoaded)
-EVENT_MANAGER:RegisterForEvent(CurrencyTracker.name, EVENT_PLAYER_ACTIVATED, CurrencyTracker.OnPlayerActivated)
