763f44736f3115c6fff1c17af0359614afd16b7b
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2021-01-28 22:41:02 +0100

    Changed version to 1.0.2

--- CurrencyTracker.lua
+++ CurrencyTracker.lua
@@ -4,7 +4,7 @@ CurrencyTracker = {}
 CurrencyTracker.name = "CurrencyTracker"
 CurrencyTracker.coloredName = "|ccc0000Curr|cff3300ency |cff9933Trac|cff9966ker|r"
 CurrencyTracker.author = "|cff6600Infenix|r"
-CurrencyTracker.version = "1.0.1"
+CurrencyTracker.version = "1.0.2"
 CurrencyTracker.website = "https://github.com/ImInfenix/CurrencyTracker"
 
 function CurrencyTracker.Initialize()
